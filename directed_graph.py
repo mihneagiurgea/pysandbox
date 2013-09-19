f808c5cac30175040922a19133ff36329fe9647d
Mihnea Giurgea <mihnea.giurgea@ubervu.com>
2013-09-19 16:45:45 +0300

    DirectedGraph.get_sccs

--- directed_graph.py
+++ directed_graph.py
@@ -45,6 +45,44 @@ class DirectedGraph(object):
         else:
             raise ValueError('Digraph has cycles')
 
+    def get_sccs(self):
+        """Find add the strongly connected components of this graph."""
+        visited = set()
+        stack = []
+
+        def dfs(node):
+            visited.add(node)
+            for i in self.outgoing[node]:
+                if i not in visited:
+                    dfs(i)
+            stack.append(node)
+
+        for i in range(1, self.N + 1):
+            if i not in visited:
+                dfs(i)
+
+        visited = set()
+        current_scc = []
+
+        def dfs_inv(node):
+            visited.add(node)
+            current_scc.append(node)
+            for i in self.incoming[node]:
+                if i not in visited:
+                    dfs_inv(i)
+
+        sccs = []
+        while stack:
+            node = stack.pop()
+            if node not in visited:
+                current_scc = []
+                dfs_inv(node)
+                # Copy the current scc into result.
+                sccs.append(current_scc)
+
+        return sccs
+
+
 if __name__ == '__main__':
     digraph = DirectedGraph.from_file('digraph.in')
-    print digraph.topological_sort()
+    print digraph.get_sccs()
