4f0797b6c657156f746293904d2a38471632ae07
Mihnea Giurgea <mihnea.giurgea@ubervu.com>
2013-11-28 16:42:13 +0200

    mtg - Moved TurnPhase to constants.py

--- mtg/turn_phase.py
+++ mtg/turn_phase.py
@@ -1,13 +1,2 @@
-class TurnPhase(object):
-
-    ### Combat phase ###
-
-    # The initial state, before attackers have been declared.
-    DeclareAttackers = 0
-    # Before blockers have been declared, but after attackers.
-    DeclareBlockers = 1
-    # CombatStep step, in which combat damage is dealt.
-    CombatStep = 2
-
-    # After the combat Phase, we'll skip directly to End of turn.
-    EndOfTurn = 3
+# Moved to constants
+from constants import TurnPhase
